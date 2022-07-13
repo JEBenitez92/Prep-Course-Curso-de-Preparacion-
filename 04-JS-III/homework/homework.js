8a740681917a3ea95819db7b3f37c7a450ce9fcc
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-07-13 19:38:43 -0300

    04-JS-III/homework/homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -68,10 +68,9 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  for (var i = 0; i < array.length; i++) {
-    if (elemento === array[i]) {
-        return true} 
-    else {return false}
+  if(array.includes(elemento)){
+    return true}
+else {return false
 }
 }
 
