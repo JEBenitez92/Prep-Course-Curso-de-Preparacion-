1b9e5d92a759d2a144062bce93a7bc60fa880a4d
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-07-13 00:19:26 -0300

    04-JS-III/homework/homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -4,7 +4,7 @@ function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
   var array = [1 , 2 , 3, 4, 5]
-  return array [0]
+  return array
 }
 
 
