b78c3bb0be1b3d0e3c5a1a314c945d875af17ba2
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-07-12 22:58:58 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -167,8 +167,10 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  for (var i = 1; i <= 10; i++)
-  { console.log(i * 6) }
+  var arreglo = []
+  for (var i = 0; i <= 10; i++) {
+      arreglo.push(6 * i) }
+  return arreglo;
 }
 
 function tieneTresDigitos(numero){
