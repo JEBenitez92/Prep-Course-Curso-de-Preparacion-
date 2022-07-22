49aa02b3e5eb3c4db397f4eb67a5ca452d078173
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-07-22 00:36:39 -0300

    04-JS-III/homework/homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -138,7 +138,9 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if( numeroDeDia === 1 || numeroDeDia === 2) {
+    return 'Es fin de semana' }
+  else { return 'Es dia Laboral' }
 } 
 
 
