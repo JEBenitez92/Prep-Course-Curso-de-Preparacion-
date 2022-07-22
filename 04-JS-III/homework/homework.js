ea0cc3950b60af13850c942e8bb1e6ef01aee278
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-07-22 00:26:33 -0300

    04-JS-III/homework/homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -123,7 +123,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var mayor18 = 0
+    for(var i = 0; i < arreglo.length; i++){
+    if(arreglo[i] > 18){
+        mayor18++ 
+    }
+    }
+    return mayor18
 }
 
 
