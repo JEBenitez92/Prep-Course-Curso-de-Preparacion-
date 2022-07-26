4eb0b4a73ebd0fc534bd4695a8cf61e2f4761ae6
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-07-26 15:21:15 -0300

    05-JS-IV/homework/homework.js

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -37,6 +37,8 @@ function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
+  var producto = objetoMisterioso *= 5
+  return producto;
 
 }
 
