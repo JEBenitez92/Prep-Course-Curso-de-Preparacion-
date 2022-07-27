35d276dcf1c2415d4cf9372f6f0062a014ccc537
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-07-27 15:09:26 -0300

    05-JS-IV/homework/homework.js

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -68,7 +68,11 @@ function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
-}
+  if( usuario[email]) {
+    return true }
+    else {return false}
+  }
+
 
 function tienePropiedad (objeto, propiedad) {
   // Devuelve "true" si el objeto (parámetro "objeto") tiene una propiedad (key) cuyo nombre es igual al valor del argumento "propiedad"
