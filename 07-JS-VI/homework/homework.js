2c128a88dae7dad4596ef6f42c85e559e2c12d94
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-07-30 00:27:07 -0300

    07-JS-VI/homework/homework.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -35,6 +35,10 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elemento, indice){
+    return elemento
+  })
+  cb(elemento)
 }
 
 function map(array, cb) {
