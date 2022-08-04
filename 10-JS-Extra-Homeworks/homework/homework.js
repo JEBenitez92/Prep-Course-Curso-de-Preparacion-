d81254484ac33cf9a9140ce01fdcb00331606007
Jonathan Benitez <Jonatan92_24@hotmail.com>
2022-08-04 15:28:01 -0300

    10-JS-Extra-Homeworks/homework/homework.js

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -68,6 +68,12 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var numeroString = numero.toString();
+    var numeroInverso = numeroString.split('');
+    var invertido = numeroInverso.reverse().join('')
+    if(numero == invertido){
+        return 'Es capicua' 
+    } else { return 'No es capicua'}
 }
 
 
